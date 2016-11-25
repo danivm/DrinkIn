69d312dad11ac1469a8b934a878a4fb5081bfb1b
Daniel Muñoz Ruiz <Daniel@MacBook-Pro.local>
2016-11-25 09:51:49 +0100

    add heroku

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser');
 
 const db = require('./db');
 const app = express()
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const routerCategories = require('./routes/categories');
 const routerAllergens = require('./routes/allergens');
